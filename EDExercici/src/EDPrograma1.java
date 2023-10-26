683fe0cd7f50146750af72d36c91b0f0b8cce687
Usuario <Usuario@DESKTOP-DGNAOIQ>
2023-10-26 12:31:27 +0200

    Primer commit Exercici 3,5 ED

---
+++ EDExercici/src/EDPrograma1.java
@@ -0,0 +1,19 @@
+
+public class EDPrograma1 {
+//Calcul quadrat
+	public static void main(String[] args) {
+		
+		int costat;
+		costat = 9;
+		int perimetre = 0;
+		int area = 0;
+		
+		perimetre = costat * 4;
+		area = costat * costat;
+		
+		System.out.println("El perimetre del quadrat es " + perimetre );
+		System.out.println("L'area del quadrat es " + area );
+
+	}
+
+}
